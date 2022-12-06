fb9af1eeb252cb5817f9d284942680f387f7d403
Lord-Valen <lord_valen@protonmail.com>
2022-12-05 21:48:04 -0500

    :recycle: `env` Add formatters

--- flake.nix
+++ flake.nix
@@ -7,27 +7,25 @@
       flake = false;
     };
 
-    nixpkgs.url = "nixpkgs";
-
     dream2nix = {
       url = "github:nix-community/dream2nix";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { dream2nix, nixpkgs, ... }:
-    let
-      projectRoot = builtins.path { path = ./.; name = "projectRoot"; };
-    in
-    dream2nix.lib.makeFlakeOutputs {
-      systems = [
-        "aarch64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
-        "x86_64-linux"
-      ];
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    dream2nix,
+    ...
+  }: let
+    projectRoot = builtins.path {
+      path = ./.;
+      name = "projectRoot";
+    };
+
+    d2nFlake = dream2nix.lib.makeFlakeOutputs {
+      systems = flake-utils.lib.defaultSystems;
       config.projectRoot = projectRoot;
       source = projectRoot;
       settings = [
@@ -36,4 +34,22 @@
         }
       ];
     };
+  in
+    dream2nix.lib.dlib.mergeFlakes [
+      d2nFlake
+      (flake-utils.lib.eachDefaultSystem (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = d2nFlake.devShells.${system}.default.overrideAttrs (old: {
+          buildInputs =
+            old.buildInputs
+            ++ [
+              pkgs.treefmt
+              pkgs.alejandra
+              pkgs.nodePackages.prettier
+              pkgs.editorconfig-checker
+            ];
+        });
+      }))
+    ];
 }
