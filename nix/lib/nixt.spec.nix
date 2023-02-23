30c7d046d2f90739ef6e340580f3477eee324c1c
Lord-Valen <lord_valen@protonmail.com>
2023-02-23 18:55:24 -0500

    :recycle: `env` Rewrite tests

--- nix/lib/nixt.spec.nix
+++ nix/lib/nixt.spec.nix
@@ -1,10 +1,15 @@
 {
   inputs,
   cell,
-}:
-cell.nixt.block ./nixt.spec.nix {
-  "nixt" = {
-    "passes this test" = 2 + 2 == 4;
-    "fails this test" = 2 + 2 == 5;
-  };
-}
+}: let
+  inherit (cell) nixt;
+in
+  nixt.block ./nixt.spec.nix {
+    "nixt" = {
+      "passes this test" = 2 + 2 == 4;
+      "fails this test" = (2 + 2 == 5) == false;
+    };
+    "grow" = {};
+    "block" = {};
+    "inject" = {};
+  }
