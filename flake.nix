69618ddc6138a9d493e4f4e4615143ea41217089
Lord-Valen <lord_valen@protonmail.com>
2023-04-01 17:23:40 -0400

    refactor: factor out types

--- flake.nix
+++ flake.nix
@@ -48,6 +48,7 @@
 
         # lib
         (functions "nixt")
+        (functions "types")
       ];
     }
     {
--- nix/lib/nixt.nix
+++ nix/lib/nixt.nix
@@ -6,34 +6,7 @@
   lib = nixpkgs.lib // builtins;
 
   inherit (std.inputs.yants) string bool path attrs list struct defun either option;
-
-  Schema = struct "Schema" {
-    __schema = string;
-    settings = Settings;
-    testSpec = list Block;
-  };
-
-  Settings = struct "Settings" {
-    list = bool;
-    watch = bool;
-    verbose = bool;
-    trace = bool;
-  };
-
-  Block = struct "Block" {
-    path = path;
-    suites = list TestSuite;
-  };
-
-  TestSuite = struct "TestSuite" {
-    name = string;
-    cases = list TestCase;
-  };
-
-  TestCase = struct "TestCase" {
-    name = string;
-    expressions = list bool;
-  };
+  inherit (cell.types) Schema Settings Block TestSuite TestCase;
 in rec {
   # Build the nixt schema. Should be outputted to __nixt for CLI consumption.
   grow =
