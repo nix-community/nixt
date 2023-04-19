e19ec7b2498777bf500f9b01433ac9affff4b7ae
Lord-Valen <lord_valen@protonmail.com>
2023-04-19 19:02:14 -0400

    chore: add prefetch commands

--- nix/repo/devshells.nix
+++ nix/repo/devshells.nix
@@ -2,14 +2,26 @@
   inputs,
   cell,
 }: let
-  inherit (inputs) std;
-  lib = inputs.nixpkgs.lib // builtins;
+  inherit (inputs) std nixpkgs;
+  lib = nixpkgs.lib // builtins;
   presets = inputs.std-data-collection.data.configs;
 in
   lib.mapAttrs (_: std.lib.dev.mkShell) {
     default = {
       name = "Node Template";
-      commands = [{package = inputs.cells.app.packages.default;}];
+      commands = [
+        {package = inputs.cells.app.packages.default;}
+        {
+          name = "prefetch";
+          help = "prefetch npm deps.";
+          command = ''${lib.getExe nixpkgs.prefetch-npm-deps} "$PRJ_ROOT"/package-lock.json | tail -1 > "$PRJ_ROOT"/nix/app/depsHash.txt'';
+        }
+        {
+          name = "upd";
+          help = "Update packages and prefetch.";
+          command = ''npm update && prefetch'';
+        }
+      ];
       nixago = with cell.configs; [
         editorconfig
         presets.conform
